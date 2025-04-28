ac599101cb89955c0dbce0eb5a3d386f7d24ebe6
bekhuli <bbbbek37@gmail.com>
2025-04-28 11:35:53 +0500

    main

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,23 @@
 package main
 
-import "github.com/bekhuli/go-todo/config"
+import (
+	"log"
+	"net/http"
+
+	"github.com/bekhuli/go-todo/config"
+	"github.com/bekhuli/go-todo/internal/db"
+	"github.com/bekhuli/go-todo/internal/router"
+)
 
 func main() {
 	cfg := config.Envs
+
+	db.Init(cfg.DatabaseURL)
+
+	r := router.NewRouter()
+
+	log.Println("Service is running on port", cfg.Port)
+	if err := http.ListenAndServe(":"+cfg.Port, r); err != nil {
+		log.Fatal(err)
+	}
 }
